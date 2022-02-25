058835d56c2e25bea3566cabf0e2fd3318925d16
sharonkorir <sharon.korir@student.moringaschool.com>
2022-02-25 19:42:20 +0300

    add api key and source url

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     ''''
     general configuration parent class
     '''
-    pass
+    NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
 
 class ProdConfig(Config):
     '''
