393edbb75c00a540fb32af49e19d0ccf7be605da
sharonkorir <sharon.korir@student.moringaschool.com>
2022-02-25 23:28:51 +0300

    display news sources on index

--- app/request.py
+++ app/request.py
@@ -1,6 +1,6 @@
 from app import app
 import urllib.request, json
-from .models import news_articles, news_sources
+from .models import news_sources
 
 Source = news_sources.Source
 
@@ -8,13 +8,15 @@ Source = news_sources.Source
 api_key = app.config['NEWS_API_KEY']
 
 #get news source url
-source_url = app.config["NEWS_API_SOURCE_URL"]
+base_url = app.config["NEWS_API_SOURCE_URL"]
+#base_url = 'https://newsapi.org/v2/top-headlines/sources?apiKey=cb57bf9b6162483bba03f1cbc7be3e87'
 
-def get_sources(source):
+def get_sources():
     '''
     Function that gets the json response to the url request
     '''
-    get_sources_url = source_url.format(source,api_key)
+    get_sources_url = base_url.format(api_key)
+    #print(get_sources_url)
 
     with urllib.request.urlopen(get_sources_url) as url:
         get_sources_data = url.read()
@@ -22,8 +24,8 @@ def get_sources(source):
 
         source_results = None
 
-        if get_sources_response['results']:
-            source_results_list = get_sources_response['results']
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
             source_results = process_results(source_results_list)
 
     return source_results
@@ -38,6 +40,7 @@ def process_results(source_list):
         source_results: a list of news source objects 
     '''
     source_results = []
+
     for source_item in source_list:
         id = source_item.get('id')
         name = source_item.get('name')
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_sources
 
 #views
 @app.route('/')
@@ -9,5 +10,8 @@ def index():
     view root page function that returns index page
     '''
 
+    #get news sources
+    news_sources = get_sources()
+    print(news_sources)
     title = 'Newsfetch - All your news in one place'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, source = news_sources)
