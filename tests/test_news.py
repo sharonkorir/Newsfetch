47ab22ab8aa9ff29b64830c86459b49b1d6b23f6
sharonkorir <sharon.korir@student.moringaschool.com>
2022-02-28 00:37:28 +0300

    add test function

--- tests/test_news.py
+++ tests/test_news.py
@@ -48,6 +48,6 @@ class ArticleTest(unittest.TestCase):
         self.assertEqual(self.new_article.source_id, "bbc-news")
         self.assertEqual(self.new_article.title, "Ukraine's Zelensky asks citizens to resist and Europe to do more")
         self.assertEqual(self.new_article.description, "Ukraine's president tells Europe it has the \"strength to stop this aggression\" and calls for action.")
-        self.assertEqual(self.new_article.artcle_url,"http://www.bbc.co.uk/news/world-europe-60527346")
+        self.assertEqual(self.new_article.article_url,"http://www.bbc.co.uk/news/world-europe-60527346")
         self.assertEqual(self.new_article.image,"https://ichef.bbci.co.uk/news/1024/branded_news/E1CA/production/_123420875_hi074132197.jpg")
         self.assertEqual(self.new_article.time, "2022-02-25T17:22:23.06428Z")
